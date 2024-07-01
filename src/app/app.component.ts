6b7d83a119565f97d44e0996c4c325e64f9eada2
Abdullah Mansoor <abdullah.mansoor@osos.om>
2024-07-01 22:13:37 +0530

    added layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,48 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { MatIcon } from '@angular/material/icon';
+import { MatIconButton } from '@angular/material/button';
+import { MatToolbar } from '@angular/material/toolbar';
+import { AsyncPipe } from '@angular/common';
+import { MatListItem, MatNavList } from '@angular/material/list';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import {
+  MatSidenav,
+  MatSidenavContainer,
+  MatSidenavContent,
+} from '@angular/material/sidenav';
+import { MatBadge } from '@angular/material/badge';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatIcon,
+    MatIconButton,
+    MatToolbar,
+    AsyncPipe,
+    MatListItem,
+    MatSidenav,
+    MatSidenavContainer,
+    MatSidenavContent,
+    MatNavList,
+    MatBadge,
+    RouterLink,
+    RouterLinkActive,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'demo-skill';
+  private breakpointObserver = inject(BreakpointObserver);
+
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,43 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./dashboard/dashboard.component').then(c => c.DashboardComponent),
+  },
+  {
+    path: 'employees',
+    loadComponent: () =>
+      import('./employees/employees.component').then(c => c.EmployeesComponent),
+  },
+  {
+    path: 'report',
+    loadComponent: () =>
+      import('./report/report.component').then(c => c.ReportComponent),
+  },
+  {
+    path: 'admin-settings',
+    loadComponent: () =>
+      import('./admin-settings/admin-settings.component').then(
+        c => c.AdminSettingsComponent
+      ),
+  },
+  {
+    path: 'help-center',
+    loadComponent: () =>
+      import('./help-center/help-center.component').then(
+        c => c.HelpCenterComponent
+      ),
+  },
+  {
+    path: 'archive',
+    loadComponent: () =>
+      import('./archive/archive.component').then(c => c.ArchiveComponent),
+  },
+];
